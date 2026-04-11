5bc4404b8b72453db9de3ef74e72b52f21c2720d
sanjay-k-alt <sanjaymandal9169@example.com>
2026-04-11 18:57:44 +0530

    fix admin login url

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axios from "axios"; 
 import { toast } from "react-toastify";
 
-const backendUrl = "https://ecommerce-app-yc1o.onrender.com";
+const backendUrl = import.meta.env.VITE_API_URL;
 
 const Login = ({ setToken }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+   
+   
 
   const onSubmitHandler = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post(
-        backendUrl + "/api/user/admin",
-        { email, password }
-      );
+      const response = await axios.post(backendUrl + "/api/user/admin", {
+        email,
+        password,
+      });
 
       if (response.data.success) {
         setToken(response.data.token);
         toast.success("Login successful ✅");
+         
       } else {
         toast.error(response.data.message);
       }
@@ -35,23 +38,31 @@ const Login = ({ setToken }) => {
         <h1 className="text-2xl font-bold mb-4">Admin Panel</h1>
 
         <form onSubmit={onSubmitHandler}>
-          <input
-            className="w-full mb-3 px-3 py-2 border"
-            type="email"
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <div className="mb-3 min-w-72">
+            <p className="text-sm mb-2">Email Address</p>
+            <input
+              className="rounded-md w-full px-3 py-2 border outline-none"
+              type="email"
+              placeholder="your@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
 
-          <input
-            className="w-full mb-3 px-3 py-2 border"
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div>
+            <p className="text-sm mb-2">Password</p>
+            <input
+              className="rounded-md w-full px-3 py-2 border outline-none"
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
 
-          <button className="w-full py-2 bg-black text-white">
+          <button className="mt-4 w-full py-2 rounded-md text-white bg-black">
             Login
           </button>
         </form>
